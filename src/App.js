c35779c7e88361288b32f463e14c7101778cb004
Deus <amadeusrm97@gmail.com>
2022-04-06 14:51:14 -0400

    login not going to home?

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import CreatePosts from './pages/CreatePosts';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+
 function App() {
+  //this will be used to determine if user is logined
+  const [isAuth, setIsAuth] = useState(false);
   return (
     <Router>
       <nav>
         <Link to='/'> Home</Link>
         <Link to='/CreatePosts'> CreatePosts</Link>
-        <Link to='/Login'> Login</Link>
+        {!isAuth && <Link to='/Login'> Login</Link> }
       </nav>
       <Routes>
-        <Route path='/' element={<Home/>}/> 
-        <Route path='/Login' element={<Login/>}/> 
-        <Route path='/CreatePosts' element={<CreatePosts/>}/> 
+        <Route path='/' element={<Home/>}/>         
+        <Route path='/CreatePosts' element={<CreatePosts/>}/>
+        <Route path='/Login' element={<Login setIsAuth={setIsAuth}/>}/>  
       </Routes>
     </Router>
   );
