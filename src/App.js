3431b6e93a33d5c146724db4503e95e319f4a4dd
Deus <amadeusrm97@gmail.com>
2022-04-06 12:31:31 -0400

    css added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-//element={}
+import Home from './pages/Home';
+import Login from './pages/Login';
+import CreatePosts from './pages/CreatePosts';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to='/'> Home</Link>
+        <Link to='/CreatePosts'> CreatePosts</Link>
+        <Link to='/Login'> Login</Link>
+      </nav>
       <Routes>
-        <Route path='/' /> 
+        <Route path='/' element={<Home/>}/> 
+        <Route path='/Login' element={<Login/>}/> 
+        <Route path='/CreatePosts' element={<CreatePosts/>}/> 
       </Routes>
     </Router>
   );
